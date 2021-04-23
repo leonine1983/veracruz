4121ecd27c49e6728637ccf28407f08908a621c4
Rogério da Silva <rogerleonino@gmail.com>
2021-04-22 21:55:10 -0300

    Criou o autoload do composer

--- index.php
+++ index.php
@@ -1,132 +1,20 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Documento sin título</title>
-	<link rel="stylesheet" href="assets/css/css_geral.css" type="text/css" />
-    <!-- Icones externos -->
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
- 	<!-- BOOTSTRAP -->
-	<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="carousel/carousel.css" rel="stylesheet">
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
+<?php 
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-</head>
-<body>
-	<header>
-    	<div id="header-div_1" class="float-left position-relative ">
-        	<!--Redes Sociais-->
-            <ul id="rede_social">
-            	<li><a href="https://www.facebook.com/Portal.Vera.Cruz/"><img src="images/redes_sociais/facebook.svg" /></a></li>
-                <li><img src="images/redes_sociais/whatsapp.svg" /></li>
-                <li><img  src="images/redes_sociais/instagam.svg" /></li>
-            </ul>
-        </div>
-        <div id="header-div_2" class="float-left position-relative ">
-			<img src="images/portal_s_circle.svg" alt="PORTAL VERA CRUZ" />
-        </div>
-        <div id="header-div_3" class="float-left position-relative">
-        	<div class="sidebar">
-            	<nav>
-                	<div><span class="material-icons">hotel</span><p>Pousadas</p></div>
-                    <div><span class="material-icons">local_dining</span><p>Restaurantes</p></div>
-                    <div><span class="material-icons">directions_run</span><p>Lazer</p></div>
-                </nav>
-            </div>
-        </div>
-        <div id="bottosn" class="position-relative box_shadow"></div>
-    </header>
-    <!-- MENU LOCALIDADES -->
-    <div id="content" class="box_shadow">
-    	<div id="menu_localidades" class="float-left" >
-        	<span>LOCALIDADES | PRAIAS</span>
-            <ul>
-            	<li>Mar Grande</li>
-                <li>Barra do Gil</li>
-                <li>Barra do Pote</li>
-            </ul>
-        </div>
-        <!-- CONTEUDO DO SITE -->
-        <div id="column_two" class="float-left">
-        	<div id="slider-show">
-            	<main role="main">
-  					<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    					<ol class="carousel-indicators">
-      						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      						<li data-target="#myCarousel" data-slide-to="1"></li>
-					    	<li data-target="#myCarousel" data-slide-to="2"></li>
-    					</ol>
-    					<div class="carousel-inner">
-      						<div class="carousel-item active">
-                    <img src="images/anuncie.svg" width="100%" height="auto">
-        						<!--
-        						<div class="container">
-          							<div class="carousel-caption text-left">
-            							<h1>Example headline.</h1>
-            							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
-          							</div>
-        						</div> -->
-      						</div>
-      						<div class="carousel-item">
-        						<svg class="bd-placeholder-img" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect width="100%" height="100%" fill="#777"/></svg>
-        						<div class="container">
-          							<div class="carousel-caption text-right">
-            							<h1>One more for good measure.</h1>
-            							<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            							<p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
-          							</div>
-        						</div>
-      						</div>
-    					</div>
-    					<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-      						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      						<span class="sr-only">Previous</span>
-    					</a>
-    					<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-      						<span class="carousel-control-next-icon" aria-hidden="true"></span>
-      						<span class="sr-only">Next</span>
-    					</a>
-  					</div>
-				</main>
-        	</div>
-        	<div id="content_flex">
-        		<aside id="content_main">
-        			<div class="titulo">
-                    	<h1>Como Anunciar</h1>
-                    </div>
-                    <hr/>
-                    <p>Você que é proprietário de algum estabelecimento no município de Vera Cruz</p>
-        		</aside>
-            	<div id="destaques">
-        			<div class="titulo">Destaques</div>
-            	<span></span>
-        	</div>
-        	</div>
-        </div>
-        <div style="clear:both"></div>
-    </div>
-    <!-- Rodapé -->
-    <footer>
-     	<h1>Portal Vera Cruz</h1>
-        <hr />
-    </footer>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-      <script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+require_once("vendor/autoload.php");
 
-</body>
-</html>
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	$sql = new Hcode\DB\Sql();
+	$results = $sql -> select ("SELECT * FROM users ");
+
+	echo json_encode($results);
+
+});
+
+$app->run();
+
+ ?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e8c60d51f8e2a0f429403921e1c20b3ab8e1f8a2',
+    'name' => 'leonine1983/veracruz',
+  ),
+  'versions' => 
+  array (
+    'leonine1983/veracruz' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e8c60d51f8e2a0f429403921e1c20b3ab8e1f8a2',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v5.2.22',
+      'version' => '5.2.22.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b18cb98131bd83103ccb26a888fdfe3177b8a663',
+    ),
+    'rain/raintpl' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5bfdf4602b8adb0e80b222fa29ac559aa9c06abf',
+    ),
+    'slim/slim' => 
+    array (
+      'pretty_version' => '2.0.0',
+      'version' => '2.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e95c9abbc57a8f023ab4dacebcab9dae4d9f1f0',
+    ),
+  ),
+);
